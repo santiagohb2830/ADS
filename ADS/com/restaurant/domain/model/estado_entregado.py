4008ffad49274f38ab928072154ce337030ddcd7
Santiago Hernandez <santiagohb2830@gmail.com>
2025-04-20 21:06:25 -0500

    Delete ADS directory

--- ADS/com/restaurant/domain/model/estado_entregado.py
+++
@@ -1,8 +0,0 @@
-from com.restaurant.domain.model.estado_pedido import EstadoPedido
-
-class EstadoEntregado(EstadoPedido):
-    def avanzar_estado(self, pedido):
-        print("El pedido ya fue entregado. No puede avanzar más.")
-
-    def nombre(self) -> str:
-        return "Entregado"
