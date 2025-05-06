ae5dd17ff5a5bb3cc77ef852650014ea2571540d
Santiago Hernandez <santiagohb2830@gmail.com>
2025-05-05 20:47:16 -0500

    Delete TallerADS directory

--- TallerADS/com/restaurant/domain/model/observador.py
+++
@@ -1,9 +0,0 @@
-# com/restaurant/domain/model/observador.py
-
-from abc import ABC, abstractmethod
-from com.restaurant.domain.model.order import Order
-
-class Observador(ABC):
-    @abstractmethod
-    def actualizar(self, pedido: Order, mensaje: str):
-        pass
--- TallerADS/main.py
+++
@@ -1,6 +0,0 @@
-# main.py
-from com.restaurant.ui.restaurant_console_app import RestaurantConsoleApp
-
-if __name__ == "__main__":
-    app = RestaurantConsoleApp()
-    app.mostrar_menu_principal()
